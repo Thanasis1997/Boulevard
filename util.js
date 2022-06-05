897d5f4f2d30c9d6c0bfc7bf9b32bd0018da5c13
Thanasis <thanasis1997abc@gmail.com>
2022-06-05 19:45:46 +0300

    test

--- util.js
+++ util.js
@@ -1,7 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    //we ready baby
-    function redir(){
-        window.location.href = "drinks.html"
+function redir(){
+    window.location.href = "drinks.html"
 }
-  });
+
    
